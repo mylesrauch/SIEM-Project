82957151a16b7aa2bc2af0f8059861c795cb1361
myles <mylesrauch@gmail.com>
2021-04-28 23:21:18 -0600

    New Axios Method thanks James...

--- frontend-nuxt-vue/pages/dummy2.vue
+++ frontend-nuxt-vue/pages/dummy2.vue
@@ -4,20 +4,47 @@
 
 <script>
 console.log(123)
-import axios from 'axios'
+
 
 export default {
+  
+// Olde Axios Function doesn't work here anymore ;(
+//   async asyncData() {
     
-  async asyncData() {
-    
-    const api = 'http://localhost:3000/all-logs'
-    // const logs = await fetch(api).then((response) => {
-    //   return response.json()
-    // }) 
-    const logs = await axios.get(api).then((response)  => {
-      return response.data
-    })
-    return { logs }
+//     const api = 'http://localhost:3000/all-logs'
+//     // const logs = await fetch(api).then((response) => {
+//     //   return response.json()
+//     // }) 
+//     const logs = await axios.get(api).then((response)  => {
+//       return response.data
+//     })
+//     return { logs }
+//   },
+  async created() {
+    this.pollData();
   },
-}
+
+  name: "Home",
+
+  data() {
+    return {
+      logs: [],
+    };
+  },
+  async fetch() {
+    this.logs = await this.$axios.$get("/api/all-logs");
+
+  },
+  methods: {
+    async pollData() {
+      this.polling = setInterval(async () => {
+        this.$fetch();
+      }, 30000);
+    },
+  },
+    beforeUnmount() {
+      clearInterval(this.polling);
+    },
+};
+
 </script>
